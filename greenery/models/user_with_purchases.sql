38141f48e3e4d125c106e5b766a47f4038292610
Paniti Thongkamphla <46415284+honpani@users.noreply.github.com>
2023-01-31 05:03:37 +0000

    initialize project

---
+++ greenery/models/user_with_purchases.sql
@@ -0,0 +1,13 @@
+WITH user_with_purchases AS (
+SELECT user_id,
+CASE WHEN COUNT(order_id) = 1 THEN '1'
+WHEN COUNT(order_id) = 2 THEN '2'
+WHEN COUNT(order_id) >= 3 THEN '3+' 
+END AS purchase 
+FROM public.orders
+GROUP BY 1
+)
+
+SELECT purchase, COUNT(1) AS user_count
+FROM user_with_purchases
+GROUP BY 1
